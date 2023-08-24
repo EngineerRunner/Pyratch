82d7abac6707600de9af562b2a984bc5e548821e
GenericProgrammer1234 <133603260+GenericProgrammer1234@users.noreply.github.com>
2023-08-24 15:37:07 +0000

    Pyratch Developer Build 3: Updated interface

--- interface.py
+++ interface.py
@@ -1,6 +1,16 @@
+import json
 import scratchattach as scr
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, abort, request
 app = Flask(__name__)
+
+class SettingsManager:
+	def __init__(self):
+		self.settings_json = json.load(open("settings.json", "r"))
+
+	def refresh_settings(self):
+		self.settings_json = json.load(open("settings.json", "r"))
+
+settings_manager = SettingsManager()
 
 def user_load(username):
 	user=scr.get_user(username)
@@ -45,33 +55,46 @@ def project_load(id):
 
 @app.route("/")
 def root():
-	return render_template("root.html")
+	return render_template("root.html", featured_projects=scr.featured_projects(), dark_mode=settings_manager.settings_json["dark-mode"])
 
 @app.route("/users/<username>")
 def user(username):
 	user=user_load(username)
-	return render_template("user.html", user=user, len=len)
+	return render_template("user.html", user=user, len=len, dark_mode=settings_manager.settings_json["dark-mode"])
 
 @app.route("/users/<username>/followers")
 def user_followers(username):
 	user=user_load(username)
-	return render_template("user_followers.html", user=user)
+	return render_template("user_followers.html", user=user, dark_mode=settings_manager.settings_json["dark-mode"])
 
 @app.route("/users/<username>/following")
 def user_following(username):
 	user=user_load(username)
-	return render_template("user_following.html", user=user)
+	return render_template("user_following.html", user=user, dark_mode=settings_manager.settings_json["dark-mode"])
 
 @app.route("/users/<username>/projects")
 def user_projects(username):
 	user=user_load(username)
-	return render_template("user_projects.html", user=user)
+	return render_template("user_projects.html", user=user, dark_mode=settings_manager.settings_json["dark-mode"])
 
 @app.route("/projects/<id>")
 def project(id):
 	project = project_load(id)
-	return render_template("project.html", project=project)
+	return render_template("project.html", project=project, turbowarp=settings_manager.settings_json["turbowarp-embed"], dark_mode=settings_manager.settings_json["dark-mode"])
 
+@app.route("/settings", methods=["POST", "GET"])
+def settings():
+	if request.method == "POST":
+		turbowarp_embed = request.form.get('turbowarp-embed')
+		dark_mode = request.form.get('dark-mode')
+		settings_newjson = json.load(open("settings.json", "r"))
+		settings_newjson["turbowarp-embed"] = True if turbowarp_embed == "on" else False
+		settings_newjson["dark-mode"] = True if dark_mode == "on" else False
+		json.dump(settings_newjson, open("settings.json", "w"), indent=4)
+		settings_manager.refresh_settings()
+		return render_template("settings.html", success=True, dark_mode=settings_manager.settings_json["dark-mode"])
+	else:
+		return render_template("settings.html", success=False, dark_mode=settings_manager.settings_json["dark-mode"])
 
 if __name__ == "__main__":
   app.run(debug=True)
